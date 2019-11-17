455ea51b973b7e3362a990baca6641b01905075b
Jack <jacklowry712@gmail.com>
2019-11-17 03:15:41 +0000

    some basic bluetooth stuff maybe?

--- scanner.py
+++ scanner.py
@@ -3,6 +3,8 @@ import wiringpi as wipi
 import time
 import requests
 import os
+import glob
+from bluetooth import *
 import time
 import json
 
@@ -31,7 +33,57 @@ for filename in os.listdir('products/'):
 
 print(curr_items)
 
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+advertise_service( server_sock, "AquaPiServer",
+                   service_id = uuid,
+                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                   profiles = [ SERIAL_PORT_PROFILE ] 
+#                   protocols = [ OBEX_UUID ] 
+                    )
 while True:
+
+	print "Waiting for connection on RFCOMM channel %d" % port
+
+	client_sock, client_info = server_sock.accept()
+	print "Accepted connection from ", client_info
+
+	try:
+	        data = client_sock.recv(1024)
+        	if len(data) == 0: break
+	        print "received [%s]" % data
+
+		if data == 'temp':
+			data = str(read_temp())+'!'
+		elif data == 'lightOn':
+			GPIO.output(17,False)
+			data = 'light on!'
+		elif data == 'lightOff':
+			GPIO.output(17,True)
+			data = 'light off!'
+		else:
+			data = 'WTF!' 
+	        client_sock.send(data)
+		print "sending [%s]" % data
+
+	except IOError:
+		pass
+
+	except KeyboardInterrupt:
+
+		print "disconnected"
+
+		client_sock.close()
+		server_sock.close()
+		print "all done"
+
+		break
 	while wipi.serialDataAvail(serial) != 0:
 		data = chr(wipi.serialGetchar(serial)) 
 		upc_code += data.strip()
